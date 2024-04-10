9f88121ce804a414d0623bc00c7561f7642d66a9
Asif Shaik <161499439+AsifShaik6177@users.noreply.github.com>
2024-04-09 21:08:58 -0500

    Created Backend Code for User Ratings

--- userrating.js
+++ userrating.js
@@ -1,51 +1,22 @@
-const express = require('express');
-const routing = express.Router();
-const service = require('../service/userrating');
+const collection = require('../Utilities/connection');
+const userRating = {};
 
-// To verify the credentials of user
-routing.get(
-    '/get-all-user-ratings',
-    (req, res, next) => {
+userRating.getAllEvents=()=>{
+    return collection.getCollection().then((users)=>{
+        return collection.getUserRatingCollection().then((userRating)=>{
+            return userRating.find({}).populate('reviewerId').populate('userId').then((results)=>{
+                return results;
+            })
+        })
+    })
+}
 
-        service.getAllUser().then(item => {
-            // Console.log( item )
-            res.json(item);
-        }).
-            catch(err => {
-                next(err);
-            });
-    }
-);
+userRating.insertNewRating=(data)=>{
+    return collection.getUserRatingCollection().then((userRating)=>{
+        return userRating.insertMany([data]).then((results)=>{
+            return results;
+        })
+    })
+}
 
-routing.put(
-    '/register-new-user-rating',
-    (req, res, next) => {
-        const userId = req.body.userId;
-        const reviewerId = req.body.reviewerId;
-        const review = req.body.review;
-        const reviewTitle = req.body.reviewTitle;
-        const reviewDetail = req.body.reviewDetail;
-        const gallery = req.body.gallery;
-        const reviewDate = req.body.reviewDate;
-
-        console.log('Inside controller');
-
-        service.insertEventRating({
-            "userId": userId,
-            "reviewerId": reviewerId,
-            "review": review,
-            "reviewTitle": reviewTitle,
-            "reviewDetail": reviewDetail,
-            "gallery": gallery,
-            "reviewDate": reviewDate
-        }).then(item => {
-            console.log(item)
-            res.json(item);
-        }).
-            catch(err => {
-                next(err);
-            });
-    }
-);
-
-module.exports = routing;
+module.exports = userRating;
