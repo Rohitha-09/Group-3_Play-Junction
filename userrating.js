041efa15aba621fd6a0d79b913d5e69add43c645
Asif Shaik <161499439+AsifShaik6177@users.noreply.github.com>
2024-04-09 21:07:19 -0500

    Created Routings for User Ratings

--- userrating.js
+++ userrating.js
@@ -1,13 +1,51 @@
-const dbLayer = require( '../models/userrating' );
+const express = require('express');
+const routing = express.Router();
+const service = require('../service/userrating');
 
-const userRating = {}
+// To verify the credentials of user
+routing.get(
+    '/get-all-user-ratings',
+    (req, res, next) => {
 
-userRating.getAllEventRatings=()=>{
-    return dbLayer.getAllEventRatings();
-}
+        service.getAllUser().then(item => {
+            // Console.log( item )
+            res.json(item);
+        }).
+            catch(err => {
+                next(err);
+            });
+    }
+);
 
-userRating.insertEventRating = (data) =>{
-    return dbLayer.insertNewRating(data);
-}
+routing.put(
+    '/register-new-user-rating',
+    (req, res, next) => {
+        const userId = req.body.userId;
+        const reviewerId = req.body.reviewerId;
+        const review = req.body.review;
+        const reviewTitle = req.body.reviewTitle;
+        const reviewDetail = req.body.reviewDetail;
+        const gallery = req.body.gallery;
+        const reviewDate = req.body.reviewDate;
 
-module.exports = userRating;
+        console.log('Inside controller');
+
+        service.insertEventRating({
+            "userId": userId,
+            "reviewerId": reviewerId,
+            "review": review,
+            "reviewTitle": reviewTitle,
+            "reviewDetail": reviewDetail,
+            "gallery": gallery,
+            "reviewDate": reviewDate
+        }).then(item => {
+            console.log(item)
+            res.json(item);
+        }).
+            catch(err => {
+                next(err);
+            });
+    }
+);
+
+module.exports = routing;
