92d8439ddb0d696a60b1170a279dca6527e21044
AnanthaParinkala <161537812+AnanthaParinkala@users.noreply.github.com>
2024-04-29 22:00:06 -0500

    separating the function call for more visibility

--- apps/play-junction-fe/src/main.ts
+++ apps/play-junction-fe/src/main.ts
@@ -1,8 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 
+const bootstrapApp = () => platformBrowserDynamic().bootstrapModule(AppModule);
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
-
+bootstrapApp().catch(console.error);
